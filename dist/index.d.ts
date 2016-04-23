dae5ddc572fcf5685ffe841f4b543b4c47a90b19
Rubén Maguregui <ruben.maguregui@gmail.com>
2016-04-23 17:06:22 -0600

    Add comments to generated TS declaration files

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,35 +1,148 @@
-export interface IEvent {
-    name: string;
-    cancelable?: boolean;
-    canceled?: boolean;
-    target?: any;
-}
-export interface IEventDispatcher {
-    addEventListener(event: string, listener: Function): any;
-    on(event: string, listener: Function): any;
-    removeEventListener(event: string, listener: Function): any;
-    off(event: string, listener: Function): any;
-    dispatchEvent(event: string | IEvent): any;
-}
-export interface IListenerManager {
-    attachEventListener(target: IEventTarget, event: string, listener: Function): any;
-    detachEventListeners(target?: IEventTarget, event?: string): any;
-}
-export declare type IEventTarget = IEventDispatcher | EventTarget;
-export declare class EventDispatcher implements IEventDispatcher {
+declare module 'wg-events' {
+  /**
+   * Describes the base structure of the events dispatched by an
+   * [[IEventDispatcher]]
+   */
+  export interface IEvent {
+    /**
+     * The name of the event
+     */
+    name:string;
+    /**
+     * Determines if the event is cancelable, so that the event dispatch loop
+     * can be interrupted by setting [[IEvent.canceled]] to true or by returning
+     * false from an event listener
+     */
+    cancelable?:boolean;
+    /**
+     * Determines if the event is canceled
+     */
+    canceled?:boolean;
+    /**
+     * The target of the event
+     */
+    target?:any;
+  }
+  /**
+   * The API that all events dispatcher must provide
+   */
+  export interface IEventDispatcher {
+    /**
+     * Adds an event listener for the indicated event
+     * @param event The event to listen to
+     * @param listener The listener to call when the event is dispatched
+     */
+    addEventListener(event:string, listener:Function):any;
+    /**
+     * An alias for [[IEventDispatcher.addEventListener]]
+     * @param event
+     * @param listener
+     */
+    on(event:string, listener:Function):any;
+    /**
+     * Removes the event listener for the indicated event
+     * @param event The event for which the listener was added
+     * @param listener The listener to remove
+     */
+    removeEventListener(event:string, listener:Function):any;
+    /**
+     * An alias for [[IEventDispatcher.removeEventListener]]
+     * @param event
+     * @param listener
+     */
+    off(event:string, listener:Function):any;
+    /**
+     * Dispatches the indicated event, initiating a dispatch loop where each
+     * of the listeners added for that event are called. The dispatch loop may
+     * be interrupted if the event is cancelable and any of the listeners cancels
+     * the event, either by returning false or by setting [[IEvent.canceled]] to
+     * true
+     * @param event
+     */
+    dispatchEvent(event:string | IEvent):any;
+  }
+  /**
+   * A helper class to allow more control over the listeners added to an event
+   * target (an implementation of [[IEventDispatcher]] or a native implementation
+   * of EventTarget)
+   */
+  export interface IListenerManager {
+    /**
+     * Attaches an event listener to the specified target (an implementation of
+     * [[IEventTarget]]). All the listeners added for the event in this way can
+     * be removed by calling [[IListenerManager.detachEventListeners]]
+     * @param target
+     * @param event
+     * @param listener
+     */
+    attachEventListener(target:IEventTarget, event:string, listener:Function):any;
+    /**
+     * Detaches the listeners for the indicated target and the indicated event.
+     * If the event is omitted, then all listeners added to the target are
+     * removed. If the target is also omitted, the all the listeners attached to
+     * any event target with this manager are removed.
+     * @param target
+     * @param event
+     */
+    detachEventListeners(target?:IEventTarget, event?:string):any;
+  }
+  /**
+   * The type for any event target. Either a [[IEventDispatcher]] or a native
+   * EventTarget
+   */
+  export type IEventTarget = IEventDispatcher | EventTarget;
+  /**
+   * The default implementation of [[IEventDispatcher]]
+   */
+  export class EventDispatcher implements IEventDispatcher {
     private listeners;
     private target;
-    constructor(target?: any);
-    addEventListener(event: string, listener: Function): void;
-    on(event: string, listener: Function): void;
-    removeEventListener(event: string, listener: Function): void;
-    off(event: string, listener: Function): void;
-    dispatchEvent(event: string | IEvent): void;
+
+    constructor(target?:any);
+
+    /**
+     * @inheritdoc
+     */
+    addEventListener(event:string, listener:Function):void;
+
+    /**
+     * @inheritdoc
+     */
+    on(event:string, listener:Function):void;
+
+    /**
+     * @inheritdoc
+     */
+    removeEventListener(event:string, listener:Function):void;
+
+    /**
+     * @inheritdoc
+     */
+    off(event:string, listener:Function):void;
+
+    /**
+     * @inheritdoc
+     */
+    dispatchEvent(event:string | IEvent):void;
+
     private injectTarget(event);
-}
-export declare class ListenerManager implements IListenerManager {
+  }
+  /**
+   * The default implementation of [[IListenerManager]]
+   */
+  export class ListenerManager implements IListenerManager {
     private attachedListeners;
+
     constructor();
-    attachEventListener(target: IEventTarget, event: string, listener: Function): void;
-    detachEventListeners(target?: IEventTarget, event?: string): void;
+
+    /**
+     * @inheritdoc
+     */
+    attachEventListener(target:IEventTarget, event:string, listener:Function):void;
+
+    /**
+     * @inheritdoc
+     */
+    detachEventListeners(target?:IEventTarget, event?:string):void;
+  }
 }
