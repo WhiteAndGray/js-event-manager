71b90fc0ffad5de88f5193337e9b870b4ed94d2c
Rubén Maguregui <ruben.maguregui@gmail.com>
2016-05-03 15:08:41 -0600

    chore(all): prepare for release 1.1.1

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
 declare module 'wg-events' {
+
   /**
    * Describes the base structure of the events dispatched by an
    * [[IEventDispatcher]]
@@ -7,21 +8,21 @@ declare module 'wg-events' {
     /**
      * The name of the event
      */
-    name: string;
+    name:string;
     /**
      * Determines if the event is cancelable, so that the event dispatch loop
      * can be interrupted by setting [[IEvent.canceled]] to true or by returning
      * false from an event listener
      */
-    cancelable?: boolean;
+    cancelable?:boolean;
     /**
      * Determines if the event is canceled
      */
-    canceled?: boolean;
+    canceled?:boolean;
     /**
      * The target of the event
      */
-    target?: any;
+    target?:any;
   }
   /**
    * The API that all events dispatcher must provide
@@ -32,25 +33,25 @@ declare module 'wg-events' {
      * @param event The event to listen to
      * @param listener The listener to call when the event is dispatched
      */
-    addEventListener(event: string, listener: Function): any;
+    addEventListener(event:string, listener:Function):any;
     /**
      * An alias for [[IEventDispatcher.addEventListener]]
      * @param event
      * @param listener
      */
-    on(event: string, listener: Function): any;
+    on(event:string, listener:Function):any;
     /**
      * Removes the event listener for the indicated event
      * @param event The event for which the listener was added
      * @param listener The listener to remove
      */
-    removeEventListener(event: string, listener: Function): any;
+    removeEventListener(event:string, listener:Function):any;
     /**
      * An alias for [[IEventDispatcher.removeEventListener]]
      * @param event
      * @param listener
      */
-    off(event: string, listener: Function): any;
+    off(event:string, listener:Function):any;
     /**
      * Dispatches the indicated event, initiating a dispatch loop where each
      * of the listeners added for that event are called. The dispatch loop may
@@ -59,7 +60,7 @@ declare module 'wg-events' {
      * true
      * @param event
      */
-    dispatchEvent(event: string | IEvent): any;
+    dispatchEvent(event:string | IEvent):any;
   }
   /**
    * A helper class to allow more control over the listeners added to an event
@@ -75,7 +76,7 @@ declare module 'wg-events' {
      * @param event
      * @param listener
      */
-    attachEventListener(target: IEventTarget, event: string, listener: Function): any;
+    attachEventListener?(target:IEventTarget, event:string, listener:Function):any;
     /**
      * Detaches the listeners for the indicated target and the indicated event.
      * If the event is omitted, then all listeners added to the target are
@@ -84,7 +85,7 @@ declare module 'wg-events' {
      * @param target
      * @param event
      */
-    detachEventListeners(target?: IEventTarget, event?: string): any;
+    detachEventListeners?(target?:IEventTarget, event?:string):any;
   }
   /**
    * The type for any event target. Either a [[IEventDispatcher]] or a native
@@ -97,27 +98,34 @@ declare module 'wg-events' {
   export class EventDispatcher implements IEventDispatcher {
     private listeners;
     private target;
-    constructor(target?: any);
+
+    constructor(target?:any);
+
     /**
      * @inheritdoc
      */
-    addEventListener(event: string, listener: Function): void;
+    addEventListener(event:string, listener:Function):void;
+
     /**
      * @inheritdoc
      */
-    on(event: string, listener: Function): void;
+    on(event:string, listener:Function):void;
+
     /**
      * @inheritdoc
      */
-    removeEventListener(event: string, listener: Function): void;
+    removeEventListener(event:string, listener:Function):void;
+
     /**
      * @inheritdoc
      */
-    off(event: string, listener: Function): void;
+    off(event:string, listener:Function):void;
+
     /**
      * @inheritdoc
      */
-    dispatchEvent(event: string | IEvent): void;
+    dispatchEvent(event:string | IEvent):void;
+
     private injectTarget(event);
   }
   /**
@@ -125,27 +133,40 @@ declare module 'wg-events' {
    */
   export class ListenerManager implements IListenerManager {
     private attachedListeners;
+
     constructor();
+
     /**
      * @inheritdoc
      */
-    attachEventListener(target: IEventTarget, event: string, listener: Function): void;
+    attachEventListener(target:IEventTarget, event:string, listener:Function):void;
+
     /**
      * @inheritdoc
      */
-    detachEventListeners(target?: IEventTarget, event?: string): void;
+    detachEventListeners(target?:IEventTarget, event?:string):void;
   }
   export class EventManager implements IEventDispatcher, IListenerManager {
     private dispatcher;
     private listenerManager;
-    constructor(target: any);
-    addEventListener(event: string, listener: Function): void;
-    on(event: string, listener: Function): void;
-    removeEventListener(event: string, listener: Function): void;
-    off(event: string, listener: Function): void;
-    dispatchEvent(event: string | IEvent): void;
-    attachEventListener(target: IEventTarget, event: string, listener: Function): void;
-    detachEventListeners(target?: IEventTarget, event?: string): void;
+
+    constructor(target:any);
+
+    addEventListener(event:string, listener:Function):void;
+
+    on(event:string, listener:Function):void;
+
+    removeEventListener(event:string, listener:Function):void;
+
+    off(event:string, listener:Function):void;
+
+    dispatchEvent(event:string | IEvent):void;
+
+    attachEventListener(target:IEventTarget, event:string, listener:Function):void;
+
+    detachEventListeners(target?:IEventTarget, event?:string):void;
+
+    forwardEvents(target:IEventDispatcher, events:string[]):void;
   }
 
 }
